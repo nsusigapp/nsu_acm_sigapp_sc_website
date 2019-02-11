a08fe2c6dfd28d5788ba091762ca02033f167893
MrScX <shovonx@protonmail.com>
2019-02-11 22:17:03 +0600

    registration page styles updated

--- public/js/listners.js
+++ public/js/listners.js
@@ -1,7 +1,18 @@
 (function(){
+    
+    // open modal;
     $(document).ready(function(){
-        $("#openSignInModal").click(function(){
+        $("#openSignInModal").click(function(event){
+            event.stopPropagation();
             $("#signInModal").fadeToggle();
-          });
+        });
+
+        // close modal on click outside of modal
+        $(document).on("click",function(event){
+            if(!$(event.target).closest("#signInModal").length){
+                $("#signInModal").fadeOut();
+            }
+        });
     });
+
 })();
